f99b31bffd82a2d60b1944521098869920b9a9d0
Mazharul Islam Tohin <89094831+toh1n@users.noreply.github.com>
2021-11-15 19:53:30 +0600

    Add files via upload

---
+++ 2_implementsThread/src/implementsThread/Main.java
@@ -0,0 +1,22 @@
+
+package implementsThread;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new NewThread();
+        
+       try{
+           for(int i=5; i>0 ; i--)
+           {
+               System.out.println("Main Thread : "+i);
+               Thread.sleep(1000);
+           }
+       }
+      catch(InterruptedException e){
+          System.out.println("Main thread interrupted.");
+      }
+        System.out.println("Main thread exiting.");
+    }
+    
+}
